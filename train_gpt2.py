d7be241ba9aeeca8d38f19857ac1c35eb353fdc4
debashishc <u4610248@anu.edu.au>
2024-08-17 22:01:31 +1000

    added causal attention, mlp and decoder block

--- train_gpt2.py
+++ train_gpt2.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import math
 
 
 @dataclass
@@ -27,6 +28,58 @@ class Block(nn.Module):
         return x
 
 
+class MLP(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, 4 * config.n_embed)
+        self.gelu = nn.GELU(
+            approximate="tanh"
+        )  # historically due to slow TF implementation
+        self.c_proj = nn.Linear(4 * config.n_embed, config.n_embed)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config) -> None:
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+        def forward(self, x):
+            B, T, C = x.size()
+
+            qkv = self.c_attn(x)
+            q, k, v = qkv.split(self.n_embd, dim=2)
+            k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+            q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+            v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
+
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+            att = F.softmax(att, dim=-1)
+
+            y = att @ v
+            y = y.transpose(1, 2).contiguous().view(B, T, C)
+
+            y = self.proj(y)
+
+
 class GPT(nn.Module):
 
     def __init__(self, config):
@@ -46,7 +99,7 @@ class GPT(nn.Module):
                 ),  # transformer blocks
                 ln_f=nn.LayerNorm(
                     config.n_embed
-                ),  # layer norm applied to the output of the last layer
+                ),  # layer norm applied to the output of the last layer, addition to original transformers architecture
             )
         )
         self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
